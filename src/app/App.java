42b4cc3f0f89c2c8eceb8638230d6cfea2b60521
DarFig <log.Dariel@gmail.com>
2017-08-14 21:19:14 +0200

    lookandfeel

--- src/app/App.java
+++ src/app/App.java
@@ -1,6 +1,8 @@
 package app;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.plaf.nimbus.*;
 
 import objects.AppWindows;
 
@@ -11,9 +13,15 @@ import objects.AppWindows;
  */
 public class App {
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(new NimbusLookAndFeel());
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
 		AppWindows principal = new AppWindows();
 		principal.setVisible(true);
 		principal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		 
 	}
 }
